263a942a689e15135c3cc34f4768309830fdbfeb
Eduardo Rico <emrs94@gmail.com>
2019-09-19 19:37:54 -0500

    ADD
    ya con ejercicios, lo metí a otra rama

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var palabra;
+
+palabra = "ocho";
+
+if (8 < palabra.length){
+    console.log("tiene menos de ocho letras");
+}else{
+    console.log("tiene mas de ocho letras u ocho letras");
+}
+
+
+
+var word;
+
+word = "ingles";
+
+if (word.includes("ing")){
+    console.log("the word have \' ingl \' in it");
+}else{
+    console.log("the word dosen\'t have \' ingl \' in it");
+}
