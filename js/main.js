f5ac3b24254cf14974a9611f7b8e9c7637836c74
Eduardo Rico <emrs94@gmail.com>
2019-09-19 19:32:15 -0500

    MOD
    eg.2 look for "ing" in a word

--- js/main.js
+++ js/main.js
@@ -2,9 +2,20 @@ var palabra;
 
 palabra = "ocho";
 
-if (8 <= palabra.length){
+if (8 < palabra.length){
     console.log("tiene menos de ocho letras");
 }else{
-    console.log("tiene mas o menos de ocho letras");
+    console.log("tiene mas de ocho letras u ocho letras");
+}
 
+
+
+var word;
+
+word = "ingles";
+
+if (word.includes("ing")){
+    console.log("the word have \' ingl \' in it");
+}else{
+    console.log("the word dosen\'t have \' ingl \' in it");
 }
