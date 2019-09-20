028f7ab057b27954667cae057fc3235c6fb2856f
Eduardo Rico <emrs94@gmail.com>
2019-09-19 19:18:43 -0500

    MOD
    -> eg_1: if lenght of a word is less than 8, print in line.

--- js/main.js
+++ js/main.js
@@ -1,68 +1,10 @@
-var age;
-var dias;
+var palabra;
 
-age = 36;
-days = age * 365;
+palabra = "ocho";
 
-console.log("edad en dias " + days);
-
-
-
-var solesConvertionFactor;
-var dolaresConvertionFactor;
-var eurosConvertionFactor;
-var pesos; 
-var sol;
-var euro;
-var dolar;
-
-solesConvertionFactor = 5.81;  //factores de conversion de XXXX a pesos
-dolaresConvertionFactor = 19.41;
-eurosConvertionFactor = 21.41; 
-
-sol = 2500; //numero de soles que tengo y quiero convertir a otras monedas.
-pesos = solesConvertionFactor * sol;
-
-euro = pesos / eurosConvertionFactor;
-dolar = pesos / dolaresConvertionFactor; 
-
-console.log(sol + " en " + "dolares en " + dolar);
-console.log(" euros : " + euro + " y en pesos " + pesos);
-
-
-
-var radio; //es el radio pues
-var area; //es el area 
-const PI = 3.1415; // es una constante pue TODO EL NOMBRE DE LA CONSTANTE DEBE DE SER EN MAYUSCULA; 
-
-radio = 10; 
-area = PI * radio * radio; //se hae la operacion
-
-console.log("El area " + area + " es de un circulo de radio " + radio); //lo imprime pues
-
-
-var numeroUno;
-numeroUno = 2;
-if (numeroUno%2 === 0){
-    console.log("es par el numero " + numeroUno)
-
+if (8 <= palabra.length){
+    console.log("tiene menos de ocho letras");
 }else{
-    console.log("el numero " + " es non")
+    console.log("tiene mas o menos de ocho letras");
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
